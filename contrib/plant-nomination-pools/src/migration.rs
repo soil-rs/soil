f8cb9c264441a699a930e2729b30804c9cc9a7ac
Wei Tang <wei@pacna.org>
2026-03-10 21:48:24 +0000

    upstream: migrating async-std to tokio (#11214)
    Minor formatting fix in nomination-pools migration.
    
    Upstream-Commit: a4fd4ede72c46500517b8979bafecfe3c9573f9c
    Upstream-PR: #11214

--- contrib/plant-nomination-pools/src/migration.rs
+++ contrib/plant-nomination-pools/src/migration.rs
@@ -1001,8 +1001,9 @@ pub mod v2 {
 			// all reward accounts must have more than ED.
 			RewardPools::<T>::iter().try_for_each(|(id, _)| -> Result<(), TryRuntimeError> {
 				ensure!(
-					<T::Currency as topsoil_core::traits::fungible::Inspect<T::AccountId>>::balance(&Pallet::<T>::generate_reward_account(id)) >=
-						T::Currency::minimum_balance(),
+					<T::Currency as topsoil_core::traits::fungible::Inspect<T::AccountId>>::balance(
+						&Pallet::<T>::generate_reward_account(id)
+					) >= T::Currency::minimum_balance(),
 					"Reward accounts must have greater balance than ED."
 				);
 				Ok(())
